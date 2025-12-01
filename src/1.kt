b5b5ea501816da7a2fdc7d7927f26a8bc1c7a7e8
T45K <tasktas9@gmail.com>
2025-12-01 23:10:43 +0900

    update

--- src/1.kt
+++ src/1.kt
@@ -1,6 +1,22 @@
 fun main() {
+    val sampleInput = readFile("1_sample.txt")
     val input = readFile("1.txt")
+
+    println("Solve 1: Sample")
+    solve1(sampleInput)
+    println()
+
+    println("Solve 1: Actual")
+    solve1(input)
+    println()
+
+    println("Solve 2: Sample")
+    solve2(sampleInput)
+    println()
+
+    println("Solve 2: Actual")
     solve2(input)
+    println()
 }
 
 fun solve1(input: List<String>) {
