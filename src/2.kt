8e5fb1e98fbb8d97c199471e6f5497fd17c509dc
T45K <tasktas9@gmail.com>
2025-12-02 23:47:25 +0900

    day2

--- src/2.kt
+++ src/2.kt
@@ -20,7 +20,30 @@ fun main() {
 }
 
 private fun solve1(input: List<String>) {
+    val ranges = input[0].split(",").map { pair -> pair.split("-").let { it[0].toLong() to it[1].toLong() } }
+    val answer = ranges.sumOf { (begin, end) ->
+        (begin..end).filter { id ->
+            val idString = id.toString()
+            val length = idString.length
+            length % 2 == 0 && idString.substring(0, length / 2) == idString.substring(length / 2)
+        }.sum()
+    }
+
+    println(answer)
 }
 
 private fun solve2(input: List<String>) {
+    val ranges = input[0].split(",").map { pair -> pair.split("-").let { it[0].toLong() to it[1].toLong() } }
+    val answer = ranges.sumOf { (begin, end) ->
+        (begin..end).filter { id ->
+            val idString = id.toString()
+            val length = idString.length
+            (1..length / 2).any { base ->
+                val div = length / base
+                length % base == 0 && (0..<div).map { idString.substring(it * base, it * base + base) }.distinct().size == 1
+            }
+        }.sum()
+    }
+
+    println(answer)
 }
