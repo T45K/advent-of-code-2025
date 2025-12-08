ac5f8192a95eb13615a40769b1d9f7619c2ea624
T45K <tasktas9@gmail.com>
2025-12-08 23:26:00 +0900

    day5

--- src/5.kt
+++ src/5.kt
@@ -19,8 +19,35 @@ fun main() {
     println()
 }
 
+private fun parseRanges(input: List<String>): List<LongRange> = input.takeWhile { it.isNotEmpty() }
+    .map {
+        val (begin, end) = it.split("-")
+        begin.toLong()..end.toLong()
+    }
+
 private fun solve1(input: List<String>) {
+    val ranges = parseRanges(input)
+    val ingredients = input.dropWhile { it.isNotEmpty() }.drop(1).map { it.toLong() }
+
+    val answer = ingredients.count { ranges.any { range -> it in range } }
+
+    println(answer)
 }
 
 private fun solve2(input: List<String>) {
+    val ranges = parseRanges(input)
+
+    val answer = ranges
+        .sortedBy { it.first }
+        .fold(0L to 0L) { (total, prevEnd), range ->
+            val begin = range.first
+            val end = range.last
+            when {
+                /* prevBegin <= begin < */ end < prevEnd -> total to prevEnd
+                begin <= prevEnd /* < end */ -> (total + end - prevEnd) to end
+                else /* prevEnd < begin < end */ -> (total + end - begin + 1) to end
+            }
+        }.first
+
+    println(answer)
 }
