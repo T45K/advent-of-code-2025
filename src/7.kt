036fb6606ebbedca5936bbc2eaf0d6958bb6200c
T45K <tasktas9@gmail.com>
2025-12-10 00:00:06 +0900

    day7

--- src/7.kt
+++ src/7.kt
@@ -20,7 +20,43 @@ fun main() {
 }
 
 private fun solve1(input: List<String>) {
+    val table = input.map { it.toCharArray() }.toTypedArray()
+    val row = table.size
+    val col = table[0].size
+    (1..<row).forEach { i ->
+        (0..<col).forEach { j ->
+            if (table[i][j] == '.' && table[i - 1][j] == 'S') table[i][j] = '|'
+            if (table[i][j] == '.' && table[i - 1][j] == '|') table[i][j] = '|'
+            if (table[i][j] == '^' && table[i - 1][j] == '|') {
+                if (j > 0) table[i][j - 1] = '|'
+                if (j < row) table[i][j + 1] = '|'
+            }
+        }
+    }
+
+    val answer = (1..<row).sumOf { i ->
+        (0..<col).count { j -> table[i][j] == '^' && table[i - 1][j] == '|' }
+    }
+
+    println(answer)
 }
 
 private fun solve2(input: List<String>) {
+    val row = input.size
+    val col = input[0].length
+    val counts = Array(row) { Array(col) { 0L } }
+    (1..<row).forEach { i ->
+        (0..<col).forEach { j ->
+            if (input[i][j] == '.' && input[i - 1][j] == 'S') counts[i][j]++
+            if (input[i][j] == '.' && counts[i - 1][j] > 0) counts[i][j] += counts[i - 1][j]
+            if (input[i][j] == '^' && counts[i - 1][j] > 0) {
+                if (j > 0) counts[i][j - 1] += counts[i - 1][j]
+                if (j < row) counts[i][j + 1] += counts[i - 1][j]
+            }
+        }
+    }
+
+    val answer = counts.last().sum()
+
+    println(answer)
 }
