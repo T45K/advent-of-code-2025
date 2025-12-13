27b129e7fb80f41fe7d4ff3c584b0cce712bba09
T45K <tasktas9@gmail.com>
2025-12-13 23:33:25 +0900

    day8

--- src/8.kt
+++ src/8.kt
@@ -20,7 +20,72 @@ fun main() {
 }
 
 private fun solve1(input: List<String>) {
+    val positions = input.map {
+        val (x, y, z) = it.split(",")
+        Triple(x.toLong(), y.toLong(), z.toLong())
+    }
+
+    val numOfNodes = input.size
+    val pairs = positions.asSequence().flatMapIndexed { index1, position1 ->
+        val pair1 = index1 to position1
+        positions.subList(index1 + 1, positions.size).mapIndexed { index2, position2 ->
+            val pair2 = index2 + index1 + 1 to position2
+            pair1 to pair2
+        }
+    }.map { (pair1, pair2) ->
+        val (index1, position1) = pair1
+        val (index2, position2) = pair2
+        Triple(index1, index2, sqDistance(position1, position2))
+    }.sortedBy { (_, _, distance) -> distance }
+        .take(numOfNodes)
+        .map { (index1, index2, _) -> index1 to index2 }.toList()
+
+    val unionFindTree = UnionFindTree(numOfNodes)
+    pairs.forEach { (index1, index2) -> unionFindTree.unit(index1, index2) }
+
+    val answer = (0..<numOfNodes).map { unionFindTree.getRoot(it) }
+        .groupingBy { it }
+        .eachCount()
+        .map { it.value }
+        .asSequence()
+        .sortedDescending()
+        .take(3)
+        .reduce { a, b -> a * b }
+    println(answer)
 }
 
+private fun sqDistance(p1: Triple<Long, Long, Long>, p2: Triple<Long, Long, Long>): Long =
+    sqDistance(p1.first - p2.first, p1.second - p2.second, p1.third - p2.third)
+
+private fun sqDistance(x: Long, y: Long, z: Long): Long =
+    x * x + y * y + z * z
+
 private fun solve2(input: List<String>) {
+    val positions = input.map {
+        val (x, y, z) = it.split(",")
+        Triple(x.toLong(), y.toLong(), z.toLong())
+    }
+
+    val pairs = positions.asSequence().flatMapIndexed { index1, position1 ->
+        val pair1 = index1 to position1
+        positions.subList(index1 + 1, positions.size).mapIndexed { index2, position2 ->
+            val pair2 = index2 + index1 + 1 to position2
+            pair1 to pair2
+        }
+    }.map { (pair1, pair2) ->
+        val (index1, position1) = pair1
+        val (index2, position2) = pair2
+        Triple(index1, index2, sqDistance(position1, position2))
+    }.sortedBy { (_, _, distance) -> distance }
+        .map { (index1, index2, _) -> index1 to index2 }.toList()
+
+    val numOfNodes = input.size
+    val unionFindTree = UnionFindTree(numOfNodes)
+
+    val (index1, index2) = pairs.asSequence()
+        .onEach { (index1, index2) -> unionFindTree.unit(index1, index2) }
+        .first { _ -> (0..<numOfNodes).map { unionFindTree.getRoot(it) }.isUnique() }
+
+    val answer = positions[index1].first * positions[index2].first
+    println(answer)
 }
--- src/util.kt
+++ src/util.kt
@@ -1,3 +1,6 @@
+import kotlin.math.max
+import kotlin.math.min
+
 fun <T> Iterable<T>.chunkedBy(predicate: (T) -> Boolean): List<List<T>> {
     val acc = mutableListOf<List<T>>()
     val tmp = mutableListOf<T>()
@@ -12,3 +15,51 @@ fun <T> Iterable<T>.chunkedBy(predicate: (T) -> Boolean): List<List<T>> {
     if (tmp.isNotEmpty()) acc.add(tmp)
     return acc
 }
+
+class UnionFindTree(numOfNodes: Int) {
+    private val nodes: IntArray = IntArray(numOfNodes) { it }
+    private val indices = ArrayDeque<Int>()
+
+    /**
+     * Returns the root of the tree to which the argument node belongs.
+     * 
+     * @param nodeNumber The node number
+     * @return The root, i.e., the smallest value in the set to which it belongs
+     */
+    fun getRoot(nodeNumber: Int): Int {
+        val rootNode = nodes[nodeNumber]
+        if (rootNode != nodeNumber) {
+            indices.add(nodeNumber)
+            return getRoot(rootNode)
+        }
+
+        indices.forEach { nodes[it] = rootNode }
+        indices.clear()
+        return nodeNumber
+    }
+
+    /**
+     * Determines whether two nodes belong to the same set.
+     * 
+     * @param nodeA Node
+     * @param nodeB Node
+     * @return Whether the two nodes belong to the same set
+     */
+    fun hasSameRoot(nodeA: Int, nodeB: Int): Boolean {
+        return getRoot(nodeA) == getRoot(nodeB)
+    }
+
+    /**
+     * Merges the sets to which the argument nodes belong.
+     * 
+     * @param nodeA Node
+     * @param nodeB Node
+     */
+    fun unit(nodeA: Int, nodeB: Int) {
+        val rootA = getRoot(nodeA)
+        val rootB = getRoot(nodeB)
+        nodes[max(rootA, rootB)] = min(rootA, rootB)
+    }
+}
+
+fun <T> Iterable<T>.isUnique(): Boolean = distinct().size == 1
